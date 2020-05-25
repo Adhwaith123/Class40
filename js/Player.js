4f623fd548738809b55a8b2e9f71017ca76ed5b9
Adhwaith123 <adhwaithkumar@gmail.com>
2020-05-25 17:58:58 +0530

    player

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
   }
 
   getCount(){
@@ -31,5 +32,15 @@ class Player {
     playerInfoRef.on("value",(data)=>{
       allPlayers = data.val();
     })
+  }
+ getCarsatend(){
+    database.ref("carsatend").on ("value",(data)=>{
+      this.rank = data.val();
+    }) 
+  }
+  static updateCars(rank){
+  database.ref ("/").update({
+    carsatend:rank
+  })  
   }
 }
